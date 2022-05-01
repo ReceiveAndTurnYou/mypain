1d7f29ce2a43bb8158896d6a5307d7748fb89a3b
Ilya Apenka <Goopera.Mail@gmail.com>
2022-05-01 21:25:28 +0300

    круд для товаров + один фикс

--- src/main/java/com/example/mypain/controllers/productsController.java
+++ src/main/java/com/example/mypain/controllers/productsController.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/products")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class productsController {
     @Autowired
     product_whRepository productWhRepository;
